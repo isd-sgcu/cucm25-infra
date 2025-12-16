078f9fd2f380a90fe00560466ddc451bac0e93ae
Tonkla W. <t0nk14.wib@gmail.com>
2025-12-16 12:14:29 +0700

    fix(script): update file names

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,11 +6,11 @@ git reset --hard
 git pull origin main
 
 echo "Pulling latest Docker images..."
-docker compose -f docker-compose.prod.yml pull
-docker compose -f docker-compose.dev.yml pull
+docker compose -f compose.prod.yml pull
+docker compose -f compose.dev.yml pull
 
 echo "Rebuilding and restarting containers..."
-docker compose -f docker-compose.prod.yml up -d
-docker compose -f docker-compose.dev.yml up -d
+docker compose -f compose.prod.yml up -d
+docker compose -f compose.dev.yml up -d
 
 echo "Update process complete."

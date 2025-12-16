15653fa1d5364ca8ecae0dfef237eb4b6ba2192a
Tonkla W. <t0nk14.wib@gmail.com>
2025-12-16 10:47:11 +0700

    chore(deployment): init commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+DOMAIN=("coin.cucm25.me" "dev-coin.cucm25.me")
+
+echo "Deploying NGINX configuration files..."
+sudo cp -r nginx/* /etc/nginx/
+
+echo "Enabling sites..."
+for site in nginx/sites-available/*; do
+    site_name=$(basename "$site")
+    sudo ln -sf /etc/nginx/sites-available/"$site_name" /etc/nginx/sites-enabled/"$site_name"
+done
+
+echo "Testing and reloading NGINX..."
+sudo nginx -t && sudo systemctl reload nginx
+
+echo "Setting up SSL certificates with Certbot..."
+sudo certbot --nginx \
+  --non-interactive --agree-tos \
+  --email "abuse@isd.sgcu.in.th" \
+  ${printf -- "-d %s " "${DOMAIN[@]}"}
+
+echo "Deployment complete."
---
+++ scripts/update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Starting update process..."
+git pull origin main
+
+echo "Pulling latest Docker images..."
+docker compose -f docker-compose.prod.yml pull
+docker compose -f docker-compose.dev.yml pull
+
+echo "Rebuilding and restarting containers..."
+docker compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.dev.yml up -d
+
+echo "Update process complete."
